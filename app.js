639f0de1546a89e058595de26effc54590e376b7
victoriya <victoriyaboyarkina@gmail.com>
2024-01-27 23:14:44 +0700

    add players, deck styles, cards animation

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+let response = {"players_count": 2, "deck": [["♠️", "J"], ["♦️", "J"], ["♣️", "9"], ["♣️", "Q"], ["♦️", "9"], ["♦️", "8"], ["♠️", "10"], ["♣️", "6"], ["♠️", "A"], ["♥️", "8"], ["♥️", "7"], ["♥️", "6"], ["♣️", "J"], ["♦️", "K"], ["♣️", "A"], ["♠️", "7"], ["♣️", "K"], ["♥️", "Q"], ["♥️", "J"], ["♣️", "7"], ["♥️", "A"], ["♦️", "10"], ["♦️", "6"], ["♠️", "9"]], "active_suit": "♠️", "attacker": [["♠️", "Q"], ["♠️", "8"], ["♦️", "7"], ["♣️", "8"], ["♠️", "6"], ["♦️", "A"]], "defender": [["♥️", "K"], ["♥️", "9"], ["♥️", "10"], ["♦️", "Q"], ["♣️", "10"], ["♠️", "K"]], "players": [[["♠️", "Q"], ["♠️", "8"], ["♦️", "7"], ["♣️", "8"], ["♠️", "6"], ["♦️", "A"]], [["♥️", "K"], ["♥️", "9"], ["♥️", "10"], ["♦️", "Q"], ["♣️", "10"], ["♠️", "K"]], [["♠️", "J"], ["♦️", "J"], ["♣️", "9"], ["♣️", "Q"], ["♦️", "9"], ["♦️", "8"]], [["♥️", "J"], ["♣️", "7"], ["♥️", "A"], ["♦️", "10"], ["♦️", "6"], ["♠️", "9"]]], "suits": ["♥️", "♦️", "♣️", "♠️"], "ranks": ["6", "7", "8", "9", "10", "J", "Q", "K", "A"], "passes": 1}
+
 const suitsMapping = {
-    "♥": 'hearts',
-    "♦": 'diamonds',
-    "♣": 'clubs',
+    "♥️": 'hearts',
+    "♦️": 'diamonds',
+    "♣️": 'clubs',
     "♠️": 'spades',
 };
 
-let response ={}
+const passesMapping = {
+    1: (image) => {
+        image.style.left = '-9px';
+    },
+    2: (image) => {
+        image.style.left = '56px';
+    },
+    3: (image) => {
+        image.style.left = '121px';
+    },
+    4: (image) => {
+        image.style.left = '186px';
+    },
+    5: (image) => {
+        image.style.left = '251px';
+    },
+};
 
-const renderCard = (card, container, cardsNumber) => {
+const renderCard = (card, container, cardsNumber, passes) => {
     const [symbol, rank] = card;
     const suit = suitsMapping[symbol];
     const image = document.createElement('img');
@@ -15,22 +33,29 @@ const renderCard = (card, container, cardsNumber) => {
     image.classList.add(`cards_number-${cardsNumber}`);
     image.classList.add(`cards_number-${cardsNumber}-hover`);
     image.src = `/img/${suit}${rank}.png`;
-    console.log(image.src)
+    // Карты кладутся на стол в зависимости от их количества, если я правильно понимаю
+    // что pass это те карты которые сейчас в игре???
+    image.addEventListener('click', () => {
+        passesMapping[passes](image);
+        image.style.top = '-256px';
+        image.classList.remove(`cards_number-${cardsNumber}-hover`);
+        image.style.transform = 'none';
+    });
     container.appendChild(image);
 };
 
-const renderBackCard = (container, cardsNumber) => {
+const renderBackCard = (container, className) => {
     const image = document.createElement('img');
     image.classList.add('card_img');
-    image.classList.add(`cards_number-${cardsNumber}`);
+    image.classList.add(className);
     image.src = '/img/card-back.png';
     container.appendChild(image);
 };
 
-const renderCards = (player, container, current) => {
+const renderCards = (player, container, current, passes) => {
     const cardsNumber = player.length;
-    current ? player.forEach((card) => renderCard(card, container, cardsNumber)) :
-        player.forEach(() => renderBackCard(container, cardsNumber));
+    current ? player.forEach((card) => renderCard(card, container, cardsNumber, passes)) :
+        player.forEach(() => renderBackCard(container, `cards_number-${cardsNumber}`));
 }
 
 const renderPlayerRoles = (players, attacker, defender) => {
@@ -44,9 +69,10 @@ const renderPlayerRoles = (players, attacker, defender) => {
     }
 };
 
-const renderPlayerName = (players) => {
-    for (let i = 0; i < players.length; i += 1) {
+const renderPlayersNames = () => {
+    for (let i = 0; i < 4; i += 1) {
         const playerNameTextEl = document.querySelector(`.player${i}-name`);
+        console.log(playerNameTextEl)
         playerNameTextEl.textContent = `player${i}`;
     }
 };
@@ -55,23 +81,32 @@ const renderDeck = (deck) => {
     const lastCard = deck[deck.length - 1];
     const container = document.querySelector('.deck_flex');
     renderCard(lastCard, container, 'lastCard');
+    deck.forEach(() => renderBackCard(container, 'deck_card'));
+    const deckCards = document.querySelectorAll('.deck_card');
+    let i = 0;
+    deckCards.forEach((image) => {
+        image.style.top = `${i * 2}px`;
+        i += 1;
+    });
 };
 
-const renderActiveSuit = (activeSuit) => {
-    const activeSuitTextEl = document.querySelector('.active_suit-text');
-    activeSuitTextEl.textContent = `Active Suit: ${activeSuit}`;
-};
+const renderPlayerCards = (players, currentPlayer, passes) => {
+    let i = 0;
+    players.forEach((player) => {
+        const playerCardsDiv = document.querySelector(`.player${i}CardsContainer`);
+        i += 1;
+        console.log(i)
+        player === currentPlayer ? renderCards(player, playerCardsDiv, true, passes) :
+        renderCards(player, playerCardsDiv, false, passes)
+    })
+
+}
 
 const render = (response) => {
     const { players_count, deck, active_suit, attacker, defender, players, suits, ranks, passes } = response;
 
-    // Рисуем карты текущего игрока
-    const player0 = players[0];
-    const player1 = players[1];
-    const player0CardsDiv = document.querySelector('.cards_container');
-    renderCards(player0, player0CardsDiv, true)
-    const player1CardsDiv = document.querySelector('.player1CardsContainer');
-    renderCards(player1, player1CardsDiv, false)
+    // Рисуем карты игроков
+    renderPlayerCards(players, players[0], passes);
 
     // Рисуем колоду
     renderDeck(deck);
@@ -80,11 +115,9 @@ const render = (response) => {
     renderPlayerRoles(players, attacker, defender)
 
     // Рисуем имена игроков
-    renderPlayerName(players);
+    renderPlayersNames();
 
-    // Рисуем козырь
-    renderActiveSuit(active_suit);
-}
+};
 
 function connect() {
   var ws = new WebSocket('ws://localhost:8765');
@@ -116,3 +149,4 @@ function connect() {
 }
 
 connect();
+render(response);
