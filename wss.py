0c75dc31a49db90dd3d14dde805ddd124f7bff7f
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-02-02 17:36:04 +0300

    Update wss.py
    for designer

--- wss.py
+++ wss.py
@@ -1,55 +1,56 @@
 #!/usr/bin/env python
 import durak_class as du
 import json
-#import pathlib
-#import ssl
+import pathlib
+import ssl
 import asyncio
 import websockets
 import json
 
-
-
-
-
-
-j1=open("durak/2.json", "r").read()
-
-j2=open("durak/3.json", "r").read()
-
-j3=open("durak/3.json", "r").read()
-
-#import requests
-
 #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 #localhost_pem = pathlib.Path(__file__).with_name("key_cert.pem")
 #ssl_context.load_cert_chain(localhost_pem)
-#resp = requests.get('url')
-#print(resp.json())
+j11=open("2.json", "r")
+j2=j11.read()
+j11.close()
+
+j22=open("3.json", "r")
+j3=j22.read()
+j22.close()
+
+j33=open("4.json", "r")
+j4=j33.read()
+j33.close()
+
+
 
 
 clients = set()
 
+
+async def socket(message):
+    for client in clients:
+            await client.send(message)
+
 async def broadcast(message):
-    
     i=len(clients)
     print(i)
+    print(message)
     if i==2:
-       
-        await asyncio.wait([client.send(json.dumps(j1))for client in clients])
+       await socket(j2)
     if i==3:
-       
-        await asyncio.wait([client.send(json.dumps(j2))for client in clients]) 
-      
+       await socket(j3)
+    if i==4:
+       await socket(j4) 
     else:
-        game = du.DurakGame(i)
-        game_state = game.play_game()
-        massmessage = json.dumps(game_state.__dict__, ensure_ascii=False)
-        await asyncio.wait([client.send(massmessage) for client in clients])
-       
-       
+        if i>=5:        
+             game = du.DurakGame(i)
+             game_state = game.play_game()
+             massmessage = json.dumps(game_state.__dict__, ensure_ascii=False)
+             await socket(massmessage)
+ 
 
 async def handle_client(websocket, path):
-    print(websocket)
     clients.add(websocket)
     try:
         async for message in websocket:
