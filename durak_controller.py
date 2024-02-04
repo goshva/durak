836182baf90e19c760b92290c47bd8235a9d2a79
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-02-04 19:12:30 +0300

    Update durak_controller.py

--- durak_controller.py
+++ durak_controller.py
@@ -1,7 +1,5 @@
-import durak_class as du
 import json
-# Example usage
-game = du.DurakGame(5)
-s1 = game.play_game()
-jsonstr1 = json.dumps(s1.__dict__)
-print(jsonstr1)
+async def socket_messj(message):
+      data=json.loads(message)
+      print(data.get("type"))
+      return data.get("type")
