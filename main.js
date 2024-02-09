dfb4453768077a4941426dc7987ba873d97f8f4a
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-02-09 19:42:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //import {html, render} from '../lit-html/lit-html.js';
-import {html, render} from'https://esm.run/lit-html@1'
-export let a=4;
+import {html, render} from'https://esm.run/lit-html@1';
 
 const suitsMapping2 = {
     'Ch': "hearts",
@@ -26,17 +25,14 @@ const passesMapping = {
         image.style.left = '251px';
     },
 };
+const A=["left:-9px","left:56px","left:121px","left:186px","left:251px"];
 
 
 
-
-
-
-
-
-class DurakGame{
-    constructor(r){
+export class DurakGame{
+    constructor(r,ws){
         let self=this;
+        self.ws=ws;//Websockets()
         self.players_count = r.players_count;
         self.deck = r.deck;
         self.active_suit = r.active_suit;
@@ -49,22 +45,57 @@ class DurakGame{
         self.target =r.target ;
         self.id =r.id ;
         self.deck_id =r.deck_id;
+        this.imgclick=this.imgclick.bind(this);
+        this.echo=this.echo.bind(this);
+        this.cash=[Array(6),Array(6),Array(6),Array(6)];//карты в игре
+        this.back=[];//отыгравшие карты
+       self.ws.onmessage=this.echo;//слушаем сообщения сервера
 }
-imgclick(e){
-   this.passes?passesMapping[passes](e.target):'';
+
+//событие карта на столе
+imgclick(e){if( e.target .style.top ==='-256px')return
+    
+    e.preventDefault
+
+   this.passes?passesMapping[this.passes](e.target):'';
    e.target .style.top = '-256px';
    e.target.classList.remove(`cards_number-${6}-hover`);
    e.target.style.transform = 'none';
-
+  let d= e.target.dataset;
+   this.w_m={type:"set","players":d.play,"pos":d.pos,"id":this.id};//отправка рендера всем
+   //this.players[d.play].splice(d.pos,1)
+   
 }
+//отправка рендера всем игрокам
+set w_m(send){
+    this.ws.send(JSON.stringify(send));
+}
+//обработчик сообщения сервера
+async echo(e){ let message=JSON.parse(e.data) ;
+(message.type==="set")?await this.echorender(message):null }
 
+//рендер игры 
+async echorender(e){
+ let  j=Number(e.players) 
+ let k=Number(e.pos)
+ 
+this.cash[j][k]=this.players[j][k];
+console.log(this.cash);
+ this.players[j].splice(k,1,null)
+ let p1=this.players[j]
+if(e.id===this.id)return
+ else { console.log('render')
+    let a=p1.map((x,i)=>{if(x!==null) return html`<img class="card_img cards_number-6" src="/img/card-back.png">`;});
+    let c=this.cash[j].map((x,i)=>{let [sym, ra] = [x[0],x[1]];
+    let suit = suitsMapping2[sym];
+    let im=`/img/${suit}${ra}.png`; 
+    return html`<img class="card_img cards_number-6 " style="top:-256px;transform:none;${A[i]}"  src =${im}>` })
+    let b=document.getElementById(e.id);
+render( html`${a}${c}`,b)};
 
-
-
-
-
-
-
+};
+    
+//стартовый рендер
 renders(){
     let s=[];
     let n=this.target; 
@@ -78,23 +109,22 @@ let img=target?pl[n].map((x,i)=>{
 let [symbol, rank] = [x[0],x[1]];
 let suit = suitsMapping2[symbol];
 let im=`/img/${suit}${rank}.png`; 
-return html`<img @click=${this.imgclick} class="card_img cards_number-6 cards_number-6-hover" data-m="play:${n},nos:${i}"  src =${im}>`})
+return html`<img @click=${this.imgclick} class="card_img cards_number-6 cards_number-6-hover r" data-play="${n}" data-pos="${i}" src =${im}>`})
   :pl[i].map((x,i)=>{return html`<img class="card_img cards_number-6" src="/img/card-back.png">`})
   let index=Math.abs(i-n)
   index=s.includes(index)?3:index;
   s.push(index)
 
 
-console.log(s)
+//console.log(s)
 let container= document.getElementById(`${index}count`)
 container.id=this.deck_id[i]
 render( html`${img}`,container)};
 };
 s=[];
-
 };
 
-const renderPlayerRoles = (players, attacker, defender,target) => {
+export const renderPlayerRoles = (players, attacker, defender,target) => {
     let s=[];
     for (let i = 0; i < players.length; i += 1) {
         let index=Math.abs(i-target)
@@ -104,7 +134,7 @@ const renderPlayerRoles = (players, attacker, defender,target) => {
         const playerRoleTextEl = document.querySelector(`.player${index}-role`);
         let a=((attacker[0][0]===players[i][0][0])&&(attacker[0][1]===players[i][0][1]))
         let b=((defender[0][0]===players[i][0][0])&&(defender[0][1]===players[i][0][1]))
-        console.log( a)
+        //console.log( a)
         if (a) {
             playerRoleTextEl.textContent = 'attacker';
         } else if (b){
@@ -114,7 +144,7 @@ const renderPlayerRoles = (players, attacker, defender,target) => {
     s=[];
 };
 
-const renderPlayersNames = () => {
+export const renderPlayersNames = () => {
     for (let i = 0; i < 4; i += 1) {
         const playerNameTextEl = document.querySelector(`.player${i}-name`);
        
@@ -122,7 +152,7 @@ const renderPlayersNames = () => {
     }
 };
 
-const renderDeck = (deck) => {
+export const renderDeck = (deck) => {
     let t = deck[deck.length - 1];
     const [symbol, rank] = [t[0],t[1]];
     const suit = suitsMapping2[symbol];
@@ -135,100 +165,9 @@ const renderDeck = (deck) => {
   let active_suit= html`<img class="card_img cards_number-lastCard cards_number-lastCard-hover" src=${im}></img>`;
     
     
-  console.log(deck)
+  //console.log(deck)
   let deckCardsmap = deck.map((m,index)=> {return html`<img class="card_img deck_card" src="/img/card-back.png" style="top:${index * 2}px ;">`})
 
 return render( html`${active_suit},${deckCardsmap}`,container)
 
 };
-
-
-
-let rendersock =async (response) => {
-    let r=response;
-    const  [players_count, deck, active_suit, attacker, defender, players, suits, ranks, passes,target] = [r.players_count, r.deck, r.active_suit, r.attacker, r.defender, r.players, r.suits, r.ranks, r.passes,r.target];
-
-    // Рисуем карты игроков
-   //renderPlayerCards(players, players[target],passes,target);
-let du=new DurakGame(r);
-du.renders();
-
-//console.log(players[0])
-    // Рисуем колоду
-     //renderDeck(deck);
-     renderDeck(deck);
-    // Рисуем роли
-     renderPlayerRoles(players, attacker, defender,target)
-
-    // Рисуем имена игроков
-     renderPlayersNames();
-
-};
-
-var ws;
-
-
-//ws.addEventListener('message',router)
-//function router(e){let data=JSON.parse(e.data);console.log(data)}
-
-
-
-
-const start_game=document.getElementById('start_game')
-start_game.addEventListener('click',function(e){ws.send(JSON.stringify({
-    type: "start"
-}));this.setAttribute('disabled',true)})
-
-
-var id_prosses=null;
-console.log(id_prosses);
-
-//console.log(start_game)
-async function connect() {
-  ws = new WebSocket('ws://localhost:8765');
-  ws.onopen = function() {
-    
-    // subscribe to some channels
-    ws.send(JSON.stringify({
-        type: "hi"
-    }));
-  };
-
-
-  ws.onmessage = async function(e) {
-    //console.log('Message:', e.data);
- let response = JSON.parse(e.data);
- //console.log(response)
- //console.log(response.deck_id)
-    if((response.id&&id_prosses===null)){id_prosses=response.id; console.log(response);}
-    
-    if((response?.deck_id)&&id_prosses){
-     console.log(response.deck_id)
-     start_game .setAttribute('disabled',true)
-    await rendersock(response);}
-    
-  };
-
-  ws.onclose = function(e) {
-    id_prosses=null;
-    console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
-
-    setTimeout(function() {
-
-      connect();
-    }, 1000);
-  };
-
-  ws.onerror = function(err) {
-    console.error('Socket encountered error: ', err.message, 'Closing socket');
-    ws.close();
-  };
-}
-
-await connect();
-//await render(response);
-
-
-
-
-   
