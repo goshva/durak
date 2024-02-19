8b8567bc2b10233f589529d61b982f04f2ba311c
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-02-19 21:47:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import {DurakGame,renderDeck,renderPlayerRoles,renderPlayersNames} from '/main.j
 
 let ws;
 
+var id_prosses;
+console.log(id_prosses);
+
 let rendersock =async (response) => {
     let r=response;
     const  [players_count, deck, active_suit, attacker, defender, players, suits, ranks, passes,target] = [r.players_count, r.deck, r.active_suit, r.attacker, r.defender, r.players, r.suits, r.ranks, r.passes,r.target];
@@ -22,54 +25,63 @@ renderDeck(deck);
 
 };
 const start_game2=document.getElementById('start_game2')
-start_game2.addEventListener('click',function(e){ws.send(JSON.stringify({
-    type: "start",n:2
-}))})
+start_game2.addEventListener('click',async function(e){ws===undefined?await connect(2,e):null;})//каждая игра идет на своем path
 
 const start_game3=document.getElementById('start_game3')
-start_game3.addEventListener('click',function(e){ws.send(JSON.stringify({
-    type: "start",n:3
-}))})
+start_game3.addEventListener('click',async function(e){ws===undefined?await connect(3,e):null})
 const start_game4=document.getElementById('start_game4')
-start_game4.addEventListener('click',function(e){ws.send(JSON.stringify({
-    type: "start",n:4
-}))})
+start_game4.addEventListener('click',async function(e){ws===undefined?await connect(4,e):null;})
+const stop_game=document.getElementById('stop_game')
+stop_game.addEventListener('click',async function(e){
+  if(window.confirm("Вы действительно хотите выйти?"))
+  {ws!==undefined?ws.close():null;}return 0})
+
+//игра начнеться когда все игроки ткнут соотв-ю кнопку
 
 
-var id_prosses=null;
-console.log(id_prosses);
 
 //console.log(start_game)
-async function connect() {
-  ws = new WebSocket('ws://localhost:8765');
-  ws.onopen = function() {
-    
+async function connect(path,e) {
+ let prBar=document.querySelector(".progress-bar")
+  e.target.style.backgroundColor='green';
+  e.target.textContent='player wait';
+  prBar.classList.add('itarget')
+
+ws = new WebSocket(`ws://localhost:8765/${path}`);
+
+ ws.onopen=async function open(e) {
+
     // subscribe to some channels
     ws.send(JSON.stringify({
-        type: "hi"
+        "type": "hi",
     }));
+
+
+    sent(path)
+    
   };
 
 
-  ws.onmessage = async function(e) {
+  ws.onmessage= async function message (e) {
     //console.log('Message:', e.data);
  let response = JSON.parse(e.data);
  //console.log(response)
  //console.log(response.deck_id)
-    if((response.id&&id_prosses===null)){id_prosses=response.id;// console.log(response);
+    if((response.id&&!id_prosses)){id_prosses=response.id;console.log(id_prosses);
+      //sent(path)
+
     }
     
     if((response?.deck_id)&&id_prosses){
      console.log(response.deck_id)
-     start_game2.setAttribute('disabled',true)
     await rendersock(response);}
     if(response.connect){let n=Number(response.connect);let buttons=[start_game2,start_game3,start_game4]
-    buttons.forEach((i,index)=>{if(index===(n-2)){i.textContent='PLAYERS';}i.setAttribute('disabled',true)})
+    buttons.forEach((i,index)=>{if(index===(n-2)){i.textContent=`PLAYERS${n}`;i.style.color='#3bff67';prBar.classList.remove('itarget')}i.setAttribute('disabled',true)})
     }
     
   };
 
-  ws.onclose = function(e) {
+  ws.onclose= async function close(e) {
     id_prosses=null;
     console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
 
@@ -79,12 +91,20 @@ async function connect() {
     }, 1000);
   };
 
-  ws.onerror = function(err) {
+ ws.onerror= async function error(err) {
     console.error('Socket encountered error: ', err.message, 'Closing socket');
     ws.close();
-  };
+};
+
+ function sent(path){ws.send(JSON.stringify({
+  type: "start",n:`${path}`
+}))}
+
 }
 
 
 
-await connect();
+//await connect(2)
+
+
+
