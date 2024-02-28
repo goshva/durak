e6e4ef179ea5338fc4aba9cb9aacf140681ff65d
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-02-28 20:39:35 +0300

    Update dum.py

--- dum.py
+++ dum.py
@@ -2,9 +2,8 @@
 #from typing import  List,Optional
 from motor.motor_asyncio import AsyncIOMotorClient
 #from pydantic import BaseModel
-
+import json
 from beanie import Document, Indexed, init_beanie
-
 
 
 
@@ -19,6 +18,7 @@ class Dum(Document):
         deck_id:list
         iid:str
         name:str
+        cach:list
 
 async def init_mongo():
     client = AsyncIOMotorClient("mongodb://localhost:27017")
@@ -32,7 +32,7 @@ async def example(du):
     await init_beanie(database=client.durak, document_models=[Dum])
    
     tonybar =Dum(deck=du.deck,active_suit=du.active_suit,attacker=du.attacker,defender=du.attacker,
-                 players=du.players,passes=du.passes,target=du.target,deck_id=du.deck_id,iid=du.id,name=du.name)
+                 players=du.players,passes=du.passes,target=du.target,deck_id=du.deck_id,iid=du.id,name=du.name,cach=[[],[],[],[]])
     await tonybar.insert()
 
 async def example_dell(name):
@@ -41,8 +41,25 @@ async def example_dell(name):
     await init_beanie(database=client.durak, document_models=[Dum])
     await Dum.find_one(Dum.name == name).delete()
 
-async def example_get(name):
+async def example_get(e):
+    if e.get("taks")==None:
    
-    client =await init_mongo()
-    await init_beanie(database=client.durak, document_models=[Dum])
-    return await Dum.find_one(Dum.name == name) 
+        client =await init_mongo()
+        await init_beanie(database=client.durak, document_models=[Dum])
+        t=str(e.get("name"))
+    #print(t)
+        game=await Dum.find_one(Dum.name == t)
+   # print(t)
+        a=int(e.get("players"))
+        b=int(e.get("pos"))
+    #c=game.players[a][b]
+        game.players[a][b]=None
+    #ind=game.players[a].index(c)
+    #print(c)
+        game.cach=e.get("cach")
+    #print(game.cach)
+    #game.players[a].pop(ind) 
+   
+        await game.save()
+    
+   
