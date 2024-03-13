89a8dad174e309304e2a26ea936e5598377544cf
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-03-14 00:25:44 +0300

    Update img_render.js

--- frontend/img_render.js
+++ frontend/img_render.js
@@ -8,96 +8,106 @@ import {animate ,flyBelow, fade,none} from '@lit-labs/motion';
     'P': "spades",
 };
 const A=["-56px","-9px","56px","121px","186px","251px"]; 
-  const a = '-250px';
+  const a = '-250';
       const fly = [
         [{transform:`translateY(0px)`}],
         [{transform:`translateY(-250px)`}],
         [{transform: `translateY(-250px) `}],
-        [{transform: `translateY(${a}) translateX(${a})`}],
+        [{transform: `translateY(${a}px) translateX(${a}px)`}],
       ]; 
 	
 	
 	const logoOptions = () => ({
             keyframeOptions: {	
-              duration: 500,
+              duration: 1000,
 			   fill: 'backwards',
             },
-            //in:fly[3] ,
-            //out:fly[1],
+            in:fly[3] ,
+            out:fly[2],
           });
+		  
+	//const wmdeff = new WeakMap();	  
  
  export function img_render(e,i){
-	 	 
-    console.log("1+:"+this.cash_back.aktive)
+	 console.log("cash:this.target"+this.cash[this.target])	 
+    console.log("1+aktive:"+this.cash_back.aktive)
+	console.log("1+back:"+this.cash_back.back)
 	
- let  j=Number(e.players) 
- let k=Number(e.pos)
- let p1=this.players[j]
+ let  j=Number(e.players); 
+ let k=Number(e.pos);
+ let p1=this.players[j];
+ var p_p=this.players[j][k];
+ var i_i=e.broken_card;
+ console.log("broken_card:"+i_i)
+ console.log("attach_card:"+p_p)
+ let rb;
  //пушим пары карт
- function sort_card(){let rb;this.cash_back.aktive.forEach((i,index,a)=>{a.splice(index,1);rb=i});
-this.cash_back.back.push({one:rb,two:this.players[j][k]}) };
-console.log(this.cash_back.back)
+ function sort_card(){this.cash_back.aktive.forEach((i,index,a)=>{if((i[0]===i_i[0])&&(i[1]===i_i[1])){a.splice(index,1);rb=i;}});
+ rb?this.cash_back.back.push({one:rb,two:this.players[j][k]}):null;}
+
 if(e.id===this.id){return}
 if(i===this._pos0){return}
- else{
+if(e.type==='round-taks'){return}
+ 
+let ps=e.passes;	
+this.Wm3.set(p_p,A[ps]);//for defender	 
 	//console.log(this._myrole) 
-   let xx=(e.role==='defender')//если мсг от кроющ
+   let xx=(e.role==='defender');//если мсг от кроющ
+   let yy=((e.role==='attacker')||(e.role==='attacker2'));//for defender
 this.passes=e.passes;
-let ps=e.passes;
-    this.cash[j][k]=this.players[j][k];
-   !xx?this.cash_back.aktive.push(this.players[j][k]):sort_card.call(this)//пушим пары карт
-    console.log("2+:"+this.cash_back.aktive)
-    console.log(this.cash_back.aktive);
-	console.log(this.cash_back.back);
+
+    this.cash[j].push(this.players[j][k]);
+   yy?this.cash_back.aktive.push(this.players[j][k]):null;//пушим пары карт//for defender
+   xx?sort_card.call(this):null;
+    console.log("2+aktive:"+this.cash_back.aktive)
+	
+	//console.log(this.Wm1.get(rb));
+    
+	console.log("11+back:"+this.cash_back.back)
      this.players[j].splice(k,1,null)   
     let a=p1.map((x,i)=>{if(x!==null) return html`<img class="card_img cards_number-6" style="top:0px;" src="./img/card-back.png">`;});
-	 
-    let c=this.cash[j].map((x,i)=>{if(x!==null){
+	console.log("cash:this..cash[j]"+this.cash[j])
+	//for attaker
+if(xx){
+	this.Wm2.set(p_p,rb);
+	
+	let y=this.cash[j];
+    let c=y.map((x,i,a)=>{if(x!==null){
+	//console.log(x)	
+	//let ww=(x===p_p)?this.Wm1.get(rb):i;
+	let ww=(this.Wm2.has(x))?this.Wm1.get(this.Wm2.get(x)):i;
+	console.log(ww);	
     let [sym, ra] = [x[0],x[1]];
     let suit = suitsMapping2[sym];
     let img=`./img/${suit}${ra}.png`; 
     return html`<img class="card_img cards_number-6 " 
-	${animate(logoOptions(i))}
-	style="transform:translateY(-250px);left:${A[(i===x.length-1)?ps:(ps===1)?ps:i]};"
-	src =${img}>` }})
-	/* let schet=0;  
-	 let csc=this.cash_back.aktive.map((x,i)=>{if(x!==null){
-		schet+=1; 
+	
+	style="transform:translateY(-250px);
+	left:${!ww?A[(i===x.length-1)?ps:(ps===1)?ps:i]:ww};"
+	src =${img}
+	${animate(logoOptions())} >` }})
+	
+ return html`${a}${c}`
+ }
+  
+if(yy){ 
+ //for defender
+ let y=this.cash[j];
+    let c=y.map((x,i,a)=>{if(x!==null){
+	let ww=(this.Wm3.get(x))?this.Wm3.get(x):null	
+	console.log(this.Wm3.get(x));
+	console.log(ww);	
     let [sym, ra] = [x[0],x[1]];
     let suit = suitsMapping2[sym];
     let img=`./img/${suit}${ra}.png`; 
     return html`<img class="card_img cards_number-6 " 
-	${animate(logoOptions(i))}
-	style="transform:translateY(-240px);left:${A[i+schet]};"
-	src =${img}>` }})
 	
-	let sort_c=[];
+	style="transform:translateY(-250px);
+	left:${!ww?A[(i===x.length-1)?ps:(ps===1)?ps:i]:ww};"
+	src =${img} 
+	${animate(logoOptions())}>` }})
 	
-	 this.cash_back.back.forEach((x,i)=>{if(x!==null){
-    let [sym, ra,sym1, ra1] = [x.one[0],x.one[1],x.two[0],x.two[1]];
-    let suit = suitsMapping2[sym];
-	let suit1 = suitsMapping2[sym1];
-    let img=`./img/${suit}${ra}.png`;
-    let img1=`./img/${suit1}${ra1}.png`;	
-    let a_img= html`<img class="card_img cards_number-6 " 
-	${animate(logoOptions(i))}
-	style="transform:translateY(-250px);left:${A[i+schet]};"
-	src =${img}>` 
-	
-let b_img= html`<img class="card_img cards_number-6 " 
-	${animate(logoOptions(i))}
-	style="transform:translateY(-280px);left:${A[i+schet]};"
-	src =${img1}>` 
-	
-	sort_c.push(a_img);sort_c.push(b_img);
-	
-	}})*/
-	
-	
-	
-	
-	
- //style="transform:translate(${A[ps]},-250px)"
- //${A[ps+1===i?ps:i]}   
-return html`${a}${c}`};
-};
+ return html`${a}${c}`
+}
+ 
+ };
