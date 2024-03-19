e1c6d29e99e5c7522b19fb5a8d8a9a42d4848da5
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-03-19 17:28:22 +0300

    Update ws_redis.py

--- ws_redis.py
+++ ws_redis.py
@@ -1,27 +1,31 @@
 import aioredis
 
 
-
-
-
-
-redis = aioredis.from_url("redis://localhost")
+redis = aioredis.from_url("redis://localhost", decode_responses=True)
     #print(redis)
 
 
 async def redisdel(key):
+    k = await redis.get(key)
+    await redis.set(k,"offline")
     await redis.delete(key)
     print(f'delete{key}')
+    print(f'delete{k}')
     
 async def redisset(key,val):
     await redis.set(key, val)
-    #print(f'set{key}')
+    await redis.set(val,"online")
+    print(f'set{key}')
    
 
 async def redisget(key):
     #await redis.set("my-key", val)
     keys = await redis.get(key)
-    print(keys)    
+    print(keys)
+    if keys:
+        return keys
+    else:
+        return "Gamer x"    
 
 
 
