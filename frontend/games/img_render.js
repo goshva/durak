f731b183b62c5934155d141a551426944f2647a2
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-03-24 14:27:46 +0300

    Update img_render.js

--- frontend/games/img_render.js
+++ frontend/games/img_render.js
@@ -23,9 +23,7 @@ import {suitsMapping2,A,passesMapping}from './static.js';
 	//const wmdeff = new WeakMap();	  
  
  export function img_render(e,i){
-	 console.log("cash:this.target"+this.cash[this.target])	 
-    console.log("1+aktive:"+this.cash_back.aktive)
-	console.log("1+back:"+this.cash_back.back)
+
 	
  let  j=Number(e.players); 
  let k=Number(e.pos);
@@ -35,41 +33,47 @@ import {suitsMapping2,A,passesMapping}from './static.js';
  console.log("broken_card:"+i_i)
  console.log("attach_card:"+p_p)
  let rb;
- //пушим пары карт
- function sort_card(){this.cash_back.aktive.forEach((i,index,a)=>{if((i[0]===i_i[0])&&(i[1]===i_i[1])){a.splice(index,1);rb=i;}});
- rb?this.cash_back.back.push({one:rb,two:this.players[j][k]}):null;}
+ 
+ function sort_card2(){this.konduktor.get_aktive().forEach((i,index,a)=>{if((i[0]===i_i[0])&&(i[1]===i_i[1])){a.splice(index,1);rb=i;}});
+ rb?this.konduktor.set_back(rb,this.players[j][k]):null;}
 
 if(e.id===this.id){return}
 if(i===this._pos0){return}
 if(e.type==='round-taks'){return}
  
-let ps=e.passes;	
-this.Wm3.set(p_p,A[ps]);//for defender	 
-	//console.log(this._myrole) 
+let ps=e.passes;
+let wm3=this.konduktor.get_wm3();	
+
+wm3.set(p_p,A[ps])	 
+	
    let xx=(e.role==='defender');//если мсг от кроющ
    let yy=((e.role==='attacker')||(e.role==='attacker2'));//for defender
 this.passes=e.passes;
 
     this.cash[j].push(this.players[j][k]);
-   yy?this.cash_back.aktive.push(this.players[j][k]):null;//пушим пары карт//for defender
-   xx?sort_card.call(this):null;
-    console.log("2+aktive:"+this.cash_back.aktive)
+   
+   yy?this.konduktor.set_aktive(this.players[j][k]):null;
+   
+   xx?sort_card2.call(this):null;
+   
 	
-	//console.log(this.Wm1.get(rb));
+	
     
-	console.log("11+back:"+this.cash_back.back)
-     this.players[j].splice(k,1,null)   
+	
+     this.players[j].splice(k,1,null)
+	 
     let a=p1.map((x,i)=>{if(x!==null) return html`<img class="card_img cards_number-6" style="top:0px;" src="./img/card-back.png">`;});
 	console.log("cash:this..cash[j]"+this.cash[j])
 	//for attaker
 if(xx){
-	this.Wm2.set(p_p,rb);
 	
+	let wm2=this.konduktor.get_wm2();
+	let wm1=this.konduktor.get_wm1();
+	wm2.set(p_p,rb)
 	let y=this.cash[j];
     let c=y.map((x,i,a)=>{if(x!==null){
-	//console.log(x)	
-	//let ww=(x===p_p)?this.Wm1.get(rb):i;
-	let ww=(this.Wm2.has(x))?this.Wm1.get(this.Wm2.get(x)):i;
+	
+	let ww=(wm2.has(x))?wm1.get(wm2.get(x)):i;
 	console.log(ww);	
     let [sym, ra] = [x[0],x[1]];
     let suit = suitsMapping2[sym];
@@ -86,10 +90,11 @@ if(xx){
   
 if(yy){ 
  //for defender
+ let wm3=this.konduktor.get_wm3();
  let y=this.cash[j];
     let c=y.map((x,i,a)=>{if(x!==null){
-	let ww=(this.Wm3.get(x))?this.Wm3.get(x):null	
-	console.log(this.Wm3.get(x));
+	let ww=(wm3.get(x))?wm3.get(x):null	
+	console.log(wm3.get(x));
 	console.log(ww);	
     let [sym, ra] = [x[0],x[1]];
     let suit = suitsMapping2[sym];
