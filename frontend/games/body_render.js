57022555258a7b98f06c4a7a604101abbbd423b9
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-04-02 17:16:29 +0300

    Update body_render.js

--- frontend/games/body_render.js
+++ frontend/games/body_render.js
@@ -20,7 +20,7 @@ function span_atr(x){let a=(x==="attacker")?span_1:(x==="defender")?span_2:null;
 	 
  let ix_text=(this._role[0]==="attacker")?"ваш ход":(this._role[0]==="attacker2")?"подкидывай карты":"вам крыться";
  let iy_text=(this._role[0]==="attacker")?"бито":(this._role[0]==="attacker2")?"бито":"беру";
-let span_0=html`<span @click=${this.taks} class="mod">${!a?iy_text:ix_text}</span>`;
+let span_0=html`<span @click=${this.taks} class="mod" style="bottom:32px;left: 104%;position: relative;">${!a?iy_text:ix_text}</span>`;
 let[p_p,rb]=this._echo?.type&&!eho?this.prerender():[null,null];	
 let n=this.players_count;	
 let left=(n>=3)?this.Img(this._pos2):null;
@@ -28,8 +28,7 @@ let right=(n===4)?this.Img(this._pos3):null;
 let header=this.Img(this._pos1);
 let footer=a||eho?this.Img(this._pos0):this.foo;//сохранить чтобы не рендерить себя до конца раунда
 a||eho?this.foo=footer:null;
-//let footer=this.Img(this._pos0)
-//let footer=this.Img(this._pos0)
+
 let section=this.renderDeck();
 let b_ack=this.b_ack;//когда кончиться колода отобразить козыря
 
@@ -41,7 +40,8 @@ ${n>=3?this.left(left,span_atr(this._role[2]),span_u2):null}
 <header  class="header">
 <div class="player1_container">
 <div id="1count" class="player1CardsContainer"><div  id=${this.deck_id[this._pos1]}>${header}</div></div>
-<h4 class="text-h4">
+<h4 class="text-h4"style="top: -69px;
+    position: relative;">
 ${span_atr.call(this,this._role[1])}
 ${span_u1}
 <span class="player1-role textA">${this._role[1]}</span>
@@ -66,8 +66,8 @@ ${span_u0}
 </h4>
 <h4 class="text-h4">
 <span class=" player0-role textA">${this._role[0]}</span>
-</h4>
-<div id="0count" class="player0CardsContainer">${span_0}<div  id=${this.deck_id[this._pos0]}>${footer}</div></div>
+</h4>${span_0}
+<div id="0count" class="player0CardsContainer"><div  id=${this.deck_id[this._pos0]}>${footer}</div></div>
 </div>
 </footer>
 </div></div>`
