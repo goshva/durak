91dd0d47766a55b6f1be155671093562d331a78b
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-04-12 19:14:42 +0300

    Update render_left.js

--- frontend/games/render_left.js
+++ frontend/games/render_left.js
@@ -3,7 +3,8 @@ import {html} from 'lit';
 
 
 export function render_left(left,sp,span_u2){return html`<div  class="left">
-<div class="player2_container">
+
+<div id="2count" class="player2CardsContainer">${left}</div>
   <div class="player-title">
     <h4 class="text-h4">
 	${sp}
@@ -14,6 +15,6 @@ export function render_left(left,sp,span_u2){return html`<div  class="left">
       <span class="player2-role textA">${this._role[2]}</span>
     </h4>
   </div>
-  <div id="2count" class="player2CardsContainer"><div id=${this.deck_id[this._pos2]}>${left}</div></div>
-</div>
+  
+
 </div>`}
