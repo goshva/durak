7aa821bb32f0cefb8e5786221493d8d0cc04da8f
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-04-12 18:47:47 +0300

    Add files via upload

---
+++ frontend/games/positing.js
@@ -0,0 +1,78 @@
+export let positing=[
+    {
+        "id": 0,
+        "name": "J",
+        "bottom": "364px",
+        "left": "27.5px",
+        "right": "118.5px",
+        "top": "-275px",
+		"top2": "-250px",
+		'transform': `translateY(-199px) rotate(270deg)`
+    },
+    {
+        "id": 1,
+        "name": "S",
+        "bottom": "364px",
+        "left": "97.5px",
+        "right": "183.5px",
+        "top": "-275px",
+		"top2": "-250px",
+		'transform': `translateY(-279px) rotate(270deg)`
+    },
+    {
+        "id": 2,
+        "name": "K",
+        "bottom": "364px",
+        "left": "160.5px",
+        "right": "248.5px",
+        "top": "-275px",
+		"top2": "-250px",
+		'transform': `translateY(-346px) rotate(270deg)`
+    },
+    {
+        "id": 3,
+        "name": "R",
+        "bottom": "364px",
+        "left": "226.5px",
+        "right": "313.5px",
+        "top": "-275px",
+		"top2": "-250px",
+		'transform': `translateY(-412px) rotate(270deg)`
+    },
+    {
+        "id": 4,
+        "name": "L",
+        "bottom": "463px",
+        "left": "61.5px",
+        "right": "118.5px",
+        "top": "-374px",
+		"top2": "-350px"
+    },
+    {
+        "id": 5,
+        "name": "P",
+        "bottom": "463px",
+        "left": "126.5px",
+        "right": "183.5px",
+        "top": "-374px",
+		"top2": "-350px"
+    },
+    {
+        "id": 6,
+        "name": "y",
+        "bottom": "463px",
+        "left": "191.5px",
+        "right": "248.5px",
+        "top": "-374px",
+		"top2": "-350px"
+    },
+    {
+        "id": 7,
+        "name": "x",
+        "bottom": "463px",
+        "left": "256.5px",
+        "right": "313.5px",
+        "top": "-374px",
+		"top2": "-350px"
+    }
+]
