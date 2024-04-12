50ceabf2c70c115f898dc6d166353c316b169171
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-04-12 19:15:53 +0300

    Update render_right.js

--- frontend/games/render_right.js
+++ frontend/games/render_right.js
@@ -2,7 +2,8 @@ import {html} from 'lit';
 
 
 export function render_right(right,sp,span_u3){return html`<div  class="right">
-<div class="player3_container">
+
+ <div id="3count" class="player3CardsContainer">${right}</div>
   <div class="player-title">
     <h4 class="text-h4">
 	${sp}
@@ -13,6 +14,6 @@ export function render_right(right,sp,span_u3){return html`<div  class="right">
       <span class="player3-role textA">${this._role[3]}</span>
     </h4>
   </div>
-  <div id="3count" class="player3CardsContainer"><div  id=${this.deck_id[this._pos3]}>${right}</div></div>
-</div>
+ 
+
 </div>`}
