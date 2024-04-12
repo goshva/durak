f51fb988f8d26b85cf486f02f7b32f962e19be6a
dimontyu <43572676+dimontyu@users.noreply.github.com>
2024-04-12 18:58:16 +0300

    Update konduktor.js

--- frontend/games/konduktor.js
+++ frontend/games/konduktor.js
@@ -4,6 +4,7 @@ export class Konduktor{
 	#Wm1;
 	#Wm2;
 	#Wm3;
+	#Wm4;
 	back;
 	aktive;
 	
@@ -16,7 +17,9 @@ this.#Wm1 = new Map();
 
 this.#Wm2 = new Map();
 
-this.#Wm3 = new Map(); 
+this.#Wm3 = new Map();
+ 
+this.#Wm4 = new Map(); 
 	
 this.broken_card=this.broken_card.bind(this);
 this.set_back=this.set_back.bind(this);
@@ -26,16 +29,16 @@ this.clearAll=this.clearAll.bind(this);
 this.get_aktive=this.get_aktive.bind(this);
 this.get_back=this.get_back.bind(this);
 this.set_aktive=this.set_aktive.bind(this);
-[this.get_wm1,this.get_wm2,this.get_wm3]=[this.get_wm1.bind(this),this.get_wm2.bind(this),this.get_wm3.bind(this)];		
+[this.get_wm1,this.get_wm2,this.get_wm3,this.get_wm4]=[this.get_wm1.bind(this),this.get_wm2.bind(this),this.get_wm3.bind(this),this.get_wm4.bind(this)];		
 }
-broken_card(){console.log('this..broken_card');return(this.back[this.back.length-1])?.one;}//битая карта
+broken_card(){/* console.log('this..broken_card'); */return(this.back[this.back.length-1])?.one;}//битая карта
 
-atack_card(){console.log('this.atack_card');return(this.back[this.back.length-1])?.two;}//atak карта
+atack_card(){/* console.log('this.atack_card'); */return(this.back[this.back.length-1])?.two;}//atak карта
 
-set_back(i,my_card){this.back.push({one:i,two:my_card})}
+set_back(i,my_card,ps){this.back.push({one:i,two:my_card,ps:ps})}
 
 
-attach(u,lft){this.#Wm1.set(u,lft);this.aktive.push(u);console.log(u)} 
+attach(u,lft){this.#Wm1.set(u,lft);this.aktive.push(u);/* console.log(u) */} 
 
 deff(){return this.#Wm3.get(this.broken_card())} 
 
@@ -44,7 +47,7 @@ get_aktive(){return this.aktive}
 get_back(){return this.back}
 
 set_aktive(u){!this.aktive.includes(u)?this.aktive.push(u):null}
-
+get_wm4(){return this.#Wm4}
 get_wm2(){return this.#Wm2}
 
 get_wm3(){return this.#Wm3}
@@ -55,6 +58,7 @@ clearAll(){
 this.#Wm1.clear();
 this.#Wm2.clear();
 this.#Wm3.clear();
+this.#Wm4.clear();
 this.back=[];
 this.aktive=[]; 
 }
